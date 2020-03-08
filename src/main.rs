0238b60e24b29b8639c55dd586c887a007b8c34c
Jiangge Zhang <tonyseek@gmail.com>
2020-03-09 05:20:55 +0800

    Implement a minimal prototype

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,66 @@
-fn main() {
-    println!("WIP");
+use kube::config::{create_client_builder, ConfigOptions};
+use openssl::nid::Nid;
+use openssl::x509::X509;
+
+type AsyncResult<T> = Result<T, Box<dyn std::error::Error>>;
+
+#[tokio::main]
+async fn main() -> AsyncResult<()> {
+    let user = whoami(Default::default()).await?;
+    println!("User: {}", user.name);
+    println!("Groups: {}", user.groups.join(", "));
+    Ok(())
+}
+
+async fn whoami(options: ConfigOptions) -> AsyncResult<UserInfo> {
+    let (_, config_loader) = create_client_builder(options).await?;
+
+    if let Some(client_pem) = config_loader.user.client_certificate_data {
+        let client_pem = X509::from_pem(&base64::decode(&client_pem)?)?;
+        UserInfo::from_x509(&client_pem)
+    } else {
+        let name = config_loader
+            .user
+            .username
+            .ok_or(Box::new(UserNotFound {}))?;
+        Ok(UserInfo::new(name, vec![]))
+    }
+}
+
+#[derive(Debug)]
+struct UserInfo {
+    pub name: String,
+    pub groups: Vec<String>,
+}
+
+impl UserInfo {
+    pub fn new(name: String, groups: Vec<String>) -> Self {
+        Self { name, groups }
+    }
+
+    pub fn from_x509(pem: &X509) -> Result<Self, Box<dyn std::error::Error>> {
+        let mut name: Option<String> = None;
+        let mut groups: Vec<String> = Vec::with_capacity(1);
+        for entry in pem.subject_name().entries() {
+            let s = entry.data().as_utf8()?;
+            match entry.object().nid() {
+                Nid::ORGANIZATIONNAME => groups.push(s.to_string()),
+                Nid::COMMONNAME => name = Some(s.to_string()),
+                _ => (),
+            }
+        }
+        let name = name.ok_or(Box::new(UserNotFound {}))?;
+        Ok(Self::new(name, groups))
+    }
+}
+
+#[derive(Debug)]
+struct UserNotFound {}
+
+impl std::error::Error for UserNotFound {}
+
+impl std::fmt::Display for UserNotFound {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "user not found")
+    }
 }
